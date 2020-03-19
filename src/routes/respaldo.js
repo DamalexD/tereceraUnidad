1801389b9d3930997f71badc85a3bd5835d69635
DamalexD <damir_mor@hotmail.com>
2020-03-19 14:12:13 -0600

    Realice los puntos 6, 7, 8 ,9 y 11

---
+++ src/routes/respaldo.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+
+exec("mongodump -d sample_airbnb -o/backup", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+
+
+//sudo node respaldo.js
