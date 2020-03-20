e3efa3b1a58e994b20921efbedcf77b7e7ee379e
DamalexD <damir_mor@hotmail.com>
2020-03-20 08:51:19 -0600

    Busqueda por id y eliminar

--- src/routes/index.js
+++ src/routes/index.js
@@ -68,6 +68,8 @@ router.get('/rentas', async(req, res)=> {
     res.json(look);
 });
 
+
+//6.- Propiedades rentadas por los clientes
 router.post('/insertar', async(req,res)=>{
         const newListing = new listingsandreviews ({
         name: req.body.name,
@@ -88,16 +90,21 @@ router.get('/busqueda', async(req,res)=>{
     res.json(constListings);
 });
 
+
+//7.- Propiedades rentadas
 router.get('/rentadosYes', async(req,res)=>{
     const rentadas = await listingsandreviews.find({$where:'this.idCustomer>0'});
     res.json(rentadas);
 });
 
+//Reporte tipo de propiedad
 router.get('/tiposPropiedad', async(req,res)=>{
     const propiedades = await listingsandreviews.find({$or:[{"property_type":"home"},{"property_type":"Department"},{"property_type":"Hotel"}]});
     res.json(propiedades);
 });
 
+
+//Reporte rango de precios
 router.get('/precios', async(req,res)=>{
     const precios = await listingsandreviews.find({$and:[{"price":{$gte:20}},{"price":{$lte:21}}]});
     res.json(precios);
@@ -116,6 +123,34 @@ router.put('/NuevoAct/:id', async(req, res) => {
     req.flash('success_msg', 'Customer Actualizado');
     res.redirect('/customer');
 });
+
+
+
+//Busquedas por nombre y pais
+
+router.get('/name/:FirstName', async(req,res)=>{
+    const name = await customer.find({"FirstName":req.params.FirstName});
+    res.json(name);
+});
+
+router.get('/country/:country', async(req,res)=>{
+    const country = await customer.find({"country":req.params.country});
+    res.json(country);
+});
+
+
+//Eliminaciones por nombre y pais
+
+router.delete('/name/delete/:FirstName', async(req,res)=>{
+    const name = await customer.remove({"FirstName":req.params.FirstName});
+    res.json(name);
+});
+
+router.delete('/country/delete/:country', async(req,res)=>{
+    const country = await customer.remove({"country":req.params.country});
+    res.json(country);
+});
+
 
 
 module.exports = router;
